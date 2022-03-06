58fff7ee39a19b2fbf829c4bc5c04fa3fbb77860
Jimmy Nordin <JimmyN@centiro.se>
2022-03-06 18:21:50 +0100

    json3

---
+++ src/StreamsAndFiles/CSV.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using Common.Model;
+
+namespace StreamsAndFiles
+{
+    public class CSV
+    {
+        private List<Movie> movies = new();
+        private string filePathSettings;
+        private string filePath;
+
+        public CSV(Settings settings)
+        {
+            filePathSettings = settings.FilePath;
+        }
+
+        public void InitList()
+        {
+            movies.Add(new Movie{Name = "The Hateful Eight", Minutes = 100});
+            movies.Add(new Movie { Name = "Django Unchained", Minutes = 110 });
+            movies.Add(new Movie { Name = "Inglourious Basterds", Minutes = 120 });
+            movies.Add(new Movie { Name = "Sin City", Minutes = 130 });
+            movies.Add(new Movie { Name = "Jackie Brown", Minutes = 140 });
+        }
+
+        public void WriteToCsvFile()
+        {
+            filePath = filePathSettings + DateTime.Now.ToString("ddMMyyyyHHmmssfff") + ".csv";
+
+            //var serializer = new CsvSerializer<IEnumerable<CsvEdiModel>>(';', true);
+            //var fileContent = new StringBuilder();
+
+            //using (var writer = new StringWriter(fileContent))
+            //{
+            //    serializer.Serialize(shipments.Select(EdiMapper.Map), writer);
+            //}
+            //File.WriteAllText(filePath, fileContent.ToString());
+
+            //UpdateShipmentStatus(shipments.Select(s => s.idSHP).ToArray(), LegacySettings);
+
+            //var mappedEdis = shipments.Select(s => ConvergenceEdiDto.Generated(s.Reference, ObjectType.Shipment));
+
+        }
+
+
+    }
+}
---
+++ src/StreamsAndFiles/JSON.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StreamsAndFiles
+{
+    public class JSON
+    {
+        private string filePath;
+
+        public JSON(Settings settings)
+        {
+            filePath = settings.FilePath;
+        }
+
+        public void WriteToFileAsJson(List<Movie> movies)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            File.WriteAllText(filePath, JsonSerializer.Serialize(movies));
+        }
+
+        public List<Movie> ReadFileAsJson()
+        {
+            string text = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Movie>>(text);
+        }
+
+    }
+}
